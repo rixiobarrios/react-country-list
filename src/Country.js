92bbabb3598313b8a862f562d11c4a4fb833b3be
James Byess <james.byess@gmail.com>
2019-02-05 23:01:44 -0500

    solution code

--- src/Country.js
+++ src/Country.js
@@ -3,22 +3,37 @@ import React, { Component } from 'react';
 class Country extends Component {
 
 	constructor() {
+		super()
+
+		this.state = {
+			capital: null
+		}
+
+		this.getCapital = this.getCapital.bind(this)
 
 	}
 
 	getCapital() {
+		fetch(`https://restcountries.eu/rest/v2/name/${this.props.name}`)
+		.then(res => res.json())
+		.then(json => {
+			console.log(json)
+			this.setState({capital: json[0].capital})
+		})
+	}
+
+	shouldComponentUpdate() {
 
 	}
 
 	render() {
 		
-		// const capital = this.props.data.capital ? <p>{this.props.data.capital}</p> : null
+		const capital = this.state.capital ? <p>{this.state.capital}</p> : null
 
 		return (
 			<div onClick={this.getCapital}>
 				<h1>{this.props.name}</h1>
-
-				{/* {capital} */}
+				{capital}
 				
 			</div>
 		);
